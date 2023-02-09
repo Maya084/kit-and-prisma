ecfd6c1cdc1b18ad1e1b250be06f4ba9af0bbe2c
Maya084 <37115588+Maya084@users.noreply.github.com>
2023-02-09 22:45:34 +0100

    ✨ Feat(articles): create new article

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,16 +13,18 @@
 
 			<button class="outline secondary"> Delete article </button>
 
-			<a href="/" class="outline contrast"> Edit article</a>
+			<a href="/" role="button" class="outline constrast" style="width: 100%"> Edit article</a>
 		</article>
 	</div>
 
-	<form actions="" method="POST">
+	<form action="?/createArticle" method="POST">
 		<h3>New article</h3>
 		<label for="title">title</label>
 		<input type="text" id="title" name="title" />
 
 		<label for="content">content</label>
 		<textarea id="content" name="content" rows="5" />
+
+		<button type="submit"> Add article</button>
 	</form>
 </div>
