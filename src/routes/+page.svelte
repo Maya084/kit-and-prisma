f337cf7a90375c2c4c7ee513a91efc6bff8bdf7f
Maya084 <37115588+Maya084@users.noreply.github.com>
2023-02-09 20:49:26 +0100

    🎉 Init: initial commit - setup picocss; add article page; install prisma

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div class="grid">
+	<div>
+		<h2>Aricles</h2>
+		<article>
+			<header>Example article</header>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod maiores tempore magnam
+				explicabo quasi beatae mollitia consequuntur delectus, repudiandae impedit distinctio quos
+				culpa obcaecati esse ab aspernatur est consequatur fugiat nulla, consectetur accusamus
+				exercitationem. Molestiae iusto, excepturi id dolor ipsa odio repudiandae doloribus,
+				obcaecati reiciendis consequatur consequuntur veritatis nobis delectus.
+			</p>
+
+			<button class="outline secondary"> Delete article </button>
+
+			<a href="/" class="outline contrast"> Edit article</a>
+		</article>
+	</div>
+
+	<form actions="" method="POST">
+		<h3>New article</h3>
+		<label for="title">title</label>
+		<input type="text" id="title" name="title" />
+
+		<label for="content">content</label>
+		<textarea id="content" name="content" rows="5" />
+	</form>
+</div>
